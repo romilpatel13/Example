6a22285f5c271c52717c829dad7949089ac6c71e
= <=romil.patel1@gmail.com>
2020-09-15 12:22:04 -0400

    Initial commit

---
+++ src/Telecom.java
@@ -0,0 +1,14 @@
+public class Telecom {
+    public static void main(String[] args)
+    {
+        int countryCode  = 91;
+
+        if(countryCode == 1){
+            System.out.println("us and Canada");
+        }
+        if(countryCode == 91){
+            System.out.println("India");
+        }
+
+    }
+}
